97689528c7236a64f1d61c3b085dcf5095d0ed89
camilaborinsky <cborinsky@itba.edu.ar>
2022-04-11 18:40:19 -0300

    parsing argument method

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # crypto-2022
 
+### Para ocultar archivo
+
 Esteganografiar el archivo de texto “mensaje1.txt” en el archivo portador “imagen1.bmp”
 obteniendo un archivo “imagenmas1 .bmp” mediante el algoritmo LSB Improved, con encripción DES en
-modo CBC con password “oculto”
-`stegobmp -embed –in “mensaje1.txt” –p “imagen1.bmp” -out “imagenmas1.bmp” –steg LSBI –a des –m cbc -pass “oculto”`
+modo CBC con password “oculto” \
+`stegobmp -embed –in “mensaje1.txt” –p “imagen1.bmp” -out “imagenmas1.bmp” –steg LSBI –a des –m cbc -pass “oculto”`\
+Esteganografiar el archivo de imagen “mensaje1.txt” en el archivo portador “imagen1 .bmp”
+obteniendo un archivo “imagenmas1.bmp” mediante el algoritmo LSB Improved, sin encripción\
+`stegobmp -embed –in “mensaje1.txt” –p “imagen1.bmp” -out “imagenmas1.bmp” –steg LSBI` \
+No se puede encriptar/desencriptar sin password. Si este dato no está, sólo se esteganografia. \
+Son válidas en cambio las siguientes opciones:
 
-`stegobmp -embed –in “mensaje1.txt” –p “imagen1.bmp” -out “imagenmas1.bmp” –steg LSBI`
+- indicar algoritmo y password pero no modo: Se asume CBC por default.
+- Indicar modo y password pero no algoritmo: Se asume aes128 por default.
+- Indicar sólo password: Se asume algoritmo aes128 en modo CBC por default. \
--- stegobmp.c
+++ stegobmp.c
@@ -6,17 +6,6 @@
 #include "structs.h"
 
 
-struct option{
-    char * name;
-    char required_arg;
-    char * arg;
-};
-
-static struct option options [] = {
-    {"extract", 0, 0},
-    {"embed", 0, 0},
-    {"steg", 1, 0}
-};
 
 int main(int argc, char const *argv[])
 {
@@ -34,13 +23,75 @@ int main(int argc, char const *argv[])
 }
 
 Parameters parse_arguments(int argc, char const *argv[]){
-    char * op_str;
-    char * op_arg;
+    Parameters params;
+    int c;
+    static struct option long_options[] =
+        {
+            {"extract",     no_argument, 0, 'x'},
+            {"embed",     no_argument, 0, 'e'},
+            {"p",  required_argument, 0, 'p'},
+            {"out",  required_argument, 0, 'o'},
+            {"in",  required_argument, 0, 'i'},
+            {"steg",  required_argument, 0, 's'},
+            {"a",    required_argument, 0, 'a'},
+            {"m",    required_argument, 0, 'm'},
+            {"pass",    required_argument, 0, 'k'},
+            {0, 0, 0, 0}
+        };
+    int option_index = 0;
 
-    
-    for(int i = 0; i < argc ; i++){
-        op_str = argv[i];
-    }
+    while (( c = getopt_long (argc, argv, 0,long_options, &option_index))>=0){
+        switch (c)
+            {
+            case 0:
+                /* If this option set a flag, do nothing else now. */
+                if (long_options[option_index].flag != 0)
+                break;
+                printf ("option %s", long_options[option_index].name);
+                if (optarg)
+                printf (" with arg %s", optarg);
+                printf ("\n");
+                break;
+
+            case 'x':
+                params.hide = 0;
+                break;
+
+            case 'e':
+                params.hide = 1;
+                break;
+
+            case 'p':
+                params.bmp_file = optarg;
+                break;
+
+            case 'o':
+                params.out_file = optarg;
+                break;
+
+            case 'i':
+                params.in_file = optarg;
+                break;
+
+            case 's':
+                params.steg = optarg;
+                break;
+
+            case 'a':
+                params.enc_alg = optarg;
+                break;
+
+            case 'm':
+                params.enc_mode = optarg;
+                break;
+
+            case 'k':
+                params.password = optarg;
+                break;
+            default:
+                abort ();
+            }
+        }
         
     
 }
